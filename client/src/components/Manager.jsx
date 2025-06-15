6c81c70e424de5eb1c6db2b41784f41f8cdab852
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2025-06-15 23:27:29 +0530

    no clue

--- client/src/components/Manager.jsx
+++ client/src/components/Manager.jsx
@@ -19,7 +19,7 @@ const Manager = () => {
   const showPassword = () => {
     alert("show the password")
     if (ref.current.src.includes('public/hide.png')) {
-      ref.current.src = "/eye.png"
+      ref.current.src = '/eye.png'
     } else {
       ref.current.src = '/hide.png'
     }
